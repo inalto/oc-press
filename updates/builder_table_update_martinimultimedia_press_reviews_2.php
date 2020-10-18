4a4e14be79f24e6ea38b545cfca806629df333bd
Alain Martini <webmaster@martini-multimedia.net>
2020-10-18 16:29:25 +0200

    show attachments added

---
+++ updates/builder_table_update_martinimultimedia_press_reviews_2.php
@@ -0,0 +1,24 @@
+<?php namespace MartiniMultimedia\Press\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateMartinimultimediaPressReviews2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('martinimultimedia_press_reviews', function($table)
+        {
+            $table->boolean('show_attachments')->nullable()->default(1);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('martinimultimedia_press_reviews', function($table)
+        {
+            $table->dropColumn('show_attachments');
+     
+        });
+    }
+}
